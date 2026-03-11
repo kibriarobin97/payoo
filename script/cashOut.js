740778f83d265a4495c8c8351c563240a7a90a3e
Golam Kibria <kibriarobin97@gmail.com>
2026-03-12 00:10:11 +0600

    added pay bill section

--- script/cashOut.js
+++ script/cashOut.js
@@ -2,12 +2,14 @@ document.getElementById("add-money-section").style.display = "block";
 document.getElementById("cash-out-section").style.display = "none";
 document.getElementById("send-money-section").style.display = "none";
 document.getElementById("get-bonus-section").style.display = "none";
+document.getElementById("bill-pay-section").style.display = "none";
 
 document.getElementById("add-money-btn").addEventListener("click", function () {
   document.getElementById("add-money-section").style.display = "block";
   document.getElementById("cash-out-section").style.display = "none";
   document.getElementById("send-money-section").style.display = "none";
   document.getElementById("get-bonus-section").style.display = "none";
+  document.getElementById("bill-pay-section").style.display = "none";
 });
 
 document.getElementById("cash-out-btn").addEventListener("click", function () {
@@ -15,6 +17,7 @@ document.getElementById("cash-out-btn").addEventListener("click", function () {
   document.getElementById("add-money-section").style.display = "none";
   document.getElementById("send-money-section").style.display = "none";
   document.getElementById("get-bonus-section").style.display = "none";
+  document.getElementById("bill-pay-section").style.display = "none";
 });
 
 document
@@ -24,11 +27,21 @@ document
     document.getElementById("add-money-section").style.display = "none";
     document.getElementById("cash-out-section").style.display = "none";
     document.getElementById("get-bonus-section").style.display = "none";
+    document.getElementById("bill-pay-section").style.display = "none";
   });
 
-document.getElementById("get-bonus-btn").addEventListener("click", function(){
-    document.getElementById("get-bonus-section").style.display = "block";
-    document.getElementById("add-money-section").style.display = "none";
-    document.getElementById("cash-out-section").style.display = "none";
-    document.getElementById("send-money-section").style.display = "none";
-})
+document.getElementById("get-bonus-btn").addEventListener("click", function () {
+  document.getElementById("get-bonus-section").style.display = "block";
+  document.getElementById("add-money-section").style.display = "none";
+  document.getElementById("cash-out-section").style.display = "none";
+  document.getElementById("send-money-section").style.display = "none";
+  document.getElementById("bill-pay-section").style.display = "none";
+});
+
+document.getElementById("bill-pay-btn").addEventListener("click", function () {
+  document.getElementById("bill-pay-section").style.display = "block";
+  document.getElementById("add-money-section").style.display = "none";
+  document.getElementById("cash-out-section").style.display = "none";
+  document.getElementById("send-money-section").style.display = "none";
+  document.getElementById("get-bonus-section").style.display = "none";
+});
