e1a77d6bf3714da8fd3d3eed6147b2a9235d8bc0
Golam Kibria <kibriarobin97@gmail.com>
2026-03-11 22:01:13 +0600

    added get bonus section

--- script/cashOut.js
+++ script/cashOut.js
@@ -1,17 +1,20 @@
 document.getElementById("add-money-section").style.display = "block";
 document.getElementById("cash-out-section").style.display = "none";
 document.getElementById("send-money-section").style.display = "none";
+document.getElementById("get-bonus-section").style.display = "none";
 
 document.getElementById("add-money-btn").addEventListener("click", function () {
   document.getElementById("add-money-section").style.display = "block";
   document.getElementById("cash-out-section").style.display = "none";
   document.getElementById("send-money-section").style.display = "none";
+  document.getElementById("get-bonus-section").style.display = "none";
 });
 
 document.getElementById("cash-out-btn").addEventListener("click", function () {
   document.getElementById("cash-out-section").style.display = "block";
   document.getElementById("add-money-section").style.display = "none";
   document.getElementById("send-money-section").style.display = "none";
+  document.getElementById("get-bonus-section").style.display = "none";
 });
 
 document
@@ -20,4 +23,12 @@ document
     document.getElementById("send-money-section").style.display = "block";
     document.getElementById("add-money-section").style.display = "none";
     document.getElementById("cash-out-section").style.display = "none";
+    document.getElementById("get-bonus-section").style.display = "none";
   });
+
+document.getElementById("get-bonus-btn").addEventListener("click", function(){
+    document.getElementById("get-bonus-section").style.display = "block";
+    document.getElementById("add-money-section").style.display = "none";
+    document.getElementById("cash-out-section").style.display = "none";
+    document.getElementById("send-money-section").style.display = "none";
+})
